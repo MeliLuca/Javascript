40a685e318c2a02caa9650774c97d7aafcd43a58
Melissari Luca <lmelissa@lab030105.dsi.unive.it>
2019-04-08 11:49:49 +0200

    aggiornamento

--- Chart/chart.js
+++ Chart/chart.js
@@ -4,24 +4,19 @@ var dataset1=[];
 var dataset2=[];
 var mainDataset=[];
 
-var N= 16;
+var N= 10;
 
-class Terremoto {
+/* class Terremoto {
     constructor(x, y, r, anno) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.anno = anno;
-    }
-}
+    } 
+}*/
 function generateNumber(min, max){
     var n= Math.floor(Math.random()*(max-min) +min);
     return n;
-}
-
-function createDatasets(){
-   
-    
 }
 
 function addData(){
@@ -30,19 +25,19 @@ function addData(){
 
     for(let i=0; i<N ; i++){
         anno+= generateNumber(0,3);
-        mainDataset.push(new Terremoto(generateNumber(-35, 35),generateNumber(-65, 65),generateNumber(4, 20), anno));   
-        console.log(mainDataset[i]);
+        mainDataset.push({ x:generateNumber(-35, 35), y: generateNumber(-65, 65), r : generateNumber(4, 20), anno: anno});   
     }
-    interval= setInterval(function (){
-        let temp= mainDataset[k];
-        console.log("valore" + temp.r);
-        temp.r > 11 ? dataset1.push(temp) : dataset2.push(temp);
+    interval= setInterval(function (){  
+        let tmp= mainDataset[k];
+        tmp['r'] > 11 ? dataset1.push(tmp) : dataset2.push(tmp);
         myChart.update();
         k++;
-        if(k> N){
+        if(k >= N){
+            console.log("la funzione dovrebbe finire");
             clearInterval(interval);
         }
     }, 1000);
+    
 } 
 
 window.onload=function(){
@@ -53,19 +48,27 @@ window.onload=function(){
         type: 'bubble',
         data: {
             datasets: [{
-                label: 'dataset1',
+                label: 'forte',
                 data: dataset1,
                 backgroundColor: 'rgba(200, 0, 0, 0.4)',
                 borderColor: 'red',
             },
             {
-                label: 'dataset2',
+                label: 'debole',
                 data: dataset2,
                 backgroundColor: 'rgba(0, 200, 0, 0.4)',
                 borderColor: 'green',
             }]
         },
         options: {
+            tooltips: {
+                callbacks: {
+                   label: function(t, d) {
+                      return  d.datasets[t.datasetIndex].label+     
+                             ': (Latitudine: ' + t.xLabel + ', Longitudine :' + t.yLabel + ', anno '+ d.datasets[t.datasetIndex].data[t.index]['anno'] +')';
+                   }
+                }
+            },
             scales: {
                 yAxes: [{
                     ticks: {
