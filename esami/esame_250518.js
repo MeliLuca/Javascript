3eace11353663ee8963f7f15a60a30f9c7f90c49
MeliLuca <lucameli12@gmail.com>
2019-04-12 12:14:25 +0200

    inserimento esami

--- esami/esame_250518.js
+++ esami/esame_250518.js
@@ -4,21 +4,35 @@
 // segnalando al giocatore che ha perso, o se sale oltre i 1000 punti, segnalando al giocatore che ha vinto. Il punteggio
 // deve essere sempre visibile. Si assuma di avere a disposizione le immagini grass.png, head.png, rear.png.
 
-var symbols={"./Texture/faccia.jpg": 50, "./Texture/culo.jpeg": -200, "./Texture/erba.jpeg": 0};
-var N = 10;
+var symbols={"./Texture/faccia.png": 50, "./Texture/culo.png": -200, "./Texture/grass.png": 0};
+var N = 5;
 var score= 0;
 
 function creaTalpa(){
-  var talpa = document.createElement("img");
-  talpa.setAttribute("height", "40");
-  talpa.setAttribute("width", "40");
-  talpa.setAttribute("src", Object.keys(symbols)[Math.floor(Math.random()*3)]);
+  var talpa= document.createElement("img");
+
+  setInterval(()=>{
+    talpa.setAttribute('src', Object.keys(symbols)[Math.floor(Math.random()*3)]);
+  },1000);
+
+  talpa.addEventListener('click', ()=>{
+    score += symbols[talpa.getAttribute('src')];
+    display.innerHTML= score;
+    if(score >= 1000){
+      document.body.innerHTML='HAI VINTO';
+    }else if(score < 0){
+      document.body.innerHTML='ti ga perso';
+    }
+  });
+
   document.body.appendChild(talpa);
 }
 
+var display= document.createElement('div');
+document.body.appendChild(display);
+
 for(let i=0; i<N; i++){
-  for(let j=0; j<N; j++){
-    creaTalpa();
-  }
-  document.body.appendChild(document.createElement("br"));
+  for(let j=0;j<N;j++)
+    creaTalpa();  
+  document.body.appendChild(document.createElement('br'));
 }
