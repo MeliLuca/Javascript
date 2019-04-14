570dc1592e2b1e2c887636bd01dd1daa5ee18d81
MeliLuca <lucameli12@gmail.com>
2019-04-14 19:18:16 +0200

    inserimento

--- esami/5_5_2017.js
+++ esami/5_5_2017.js
@@ -1,25 +1,20 @@
 window.onload=function(){
-    var headerList=[]
+     var headerList=[]
     for(let i=1; i<7; i++){  
-        headerList[i]=[];
-        headerList[i].push(document.getElementsByTagName('h'+ i.toString()));
+        headerList[i]=document.getElementsByTagName('h'+ i.toString());
     }
     for(let i=1; i<7; i++){
         var currHead= headerList[i];
-        var list= currHead[0]
         console.log(currHead);
-        while(list.length > 0){
-            var childs = list.childsNode;
+        for(let j=0; j< currHead.length; j++){
 
-            console.log(childs)
-            for(let j=0; j< childs.length; j++){
-                childs[0].innerHTML.toUpperCase();
-            }
+            currHead[j].textContent= currHead[j].textContent.toUpperCase();
+
             var level = i<6 ? i+1 : i;
-            var nuovo = document.createElement('h'+level);
-            nuovo.innerHTML= currHead[0].innerHTML.toUpperCase();
-            currHead[0].parentNode.insertBefore(nuovo, currHead[0]);
-            currHead[0].parentNode.removeChild(currHead[0]);
+            var nuovo = document.createElement('h'+level.toString());
+            nuovo.innerHTML= currHead[j].innerHTML;
+            currHead[j].parentNode.insertBefore(nuovo, currHead[j]);
+            currHead[j].parentNode.removeChild(currHead[j]);
         }
-    }
+    }  
 }
