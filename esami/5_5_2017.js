b49ad3bd26e43845eb409d92c4546c57cdd17660
MeliLuca <lucameli12@gmail.com>
2019-04-15 20:03:14 +0200

    inserimento

--- esami/5_5_2017.js
+++ esami/5_5_2017.js
@@ -5,16 +5,16 @@ window.onload=function(){
     }
     for(let i=1; i<7; i++){
         var currHead= headerList[i];
-        console.log(currHead);
         for(let j=0; j< currHead.length; j++){
-
-            currHead[j].textContent= currHead[j].textContent.toUpperCase();
-
-            var level = i<6 ? i+1 : i;
-            var nuovo = document.createElement('h'+level.toString());
-            nuovo.innerHTML= currHead[j].innerHTML;
-            currHead[j].parentNode.insertBefore(nuovo, currHead[j]);
-            currHead[j].parentNode.removeChild(currHead[j]);
+            currHead[j].textContent=currHead[j].textContent.toUpperCase();
+            if(i<6){
+                var level = i+1;
+                var nuovo = document.createElement('h' + level.toString());
+                console.log(nuovo)
+                nuovo.innerHTML= currHead[j].innerHTML;
+                currHead[j].parentNode.insertBefore(nuovo, currHead[j]);
+                currHead[j].parentNode.removeChild(currHead[j]);
+            }
         }
     }  
 }
